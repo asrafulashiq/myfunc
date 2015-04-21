fd32c61d68fd2327459a3fb8d83e9145fc8c24ab
asrafulashiq <asrafulashiq@gmail.com>
2015-04-21 16:01:18 +0600

    a

--- myaudio.py
+++ myaudio.py
@@ -10,6 +10,7 @@ import pyaudio
 import wave
 import sys
 import recordaudio
+import play
 
 def record(*args,**kwargs):
 	return recordaudio.record(*args,**kwargs)
@@ -22,20 +23,7 @@ def play(file,chunk=1024):
 			chunk : default 1024
 			file : audio file name to be played, like .wav
 	"""
-
-	wf = wave.open(file, 'rb')
-	p = pyaudio.PyAudio()
-	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-	data = wf.readframes(chunk)
-	while data != '':
-		stream.write(data)
-		data = wf.readframes(chunk)
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
+	play.play(file,chunk)
 
 
 
