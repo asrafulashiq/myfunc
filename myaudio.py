879836e56815a41bdb1f3f78b25c7811727a0f7e
asrafulashiq <asrafulashiq@gmail.com>
2015-04-21 15:26:18 +0600

    commiting

---
+++ myaudio.py
@@ -0,0 +1,43 @@
+"""
+	This package has some functions to record or play 
+	audio file..
+	For recording call record().
+	For playing call play().
+
+"""
+
+import pyaudio
+import wave
+import sys
+import recordaudio
+
+
+def play(file,chunk=1024):
+	"""	
+		play an audio file from a file
+		input argumets:
+			chunk : default 1024
+			file : audio file name to be played, like .wav
+	"""
+
+	wf = wave.open(file, 'rb')
+	p = pyaudio.PyAudio()
+	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+	data = wf.readframes(chunk)
+	while data != '':
+		stream.write(data)
+		data = wf.readframes(chunk)
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+def main():
+	recordaudio.record(file="my.wav")
+	play("my.wav")
+if __name__ == '__main__':
+	main()
+ 
+	
